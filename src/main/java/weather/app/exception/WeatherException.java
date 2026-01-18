2eb9cfc1935e26d806787555fa2ad4a7ec770d62
Ion Schimbator <ionschimbator98@gmail.com>
2026-01-18 13:36:49 +0200

    feat: add constructor with cause for exception chaining

--- src/main/java/weather/app/exception/WeatherException.java
+++ src/main/java/weather/app/exception/WeatherException.java
@@ -5,4 +5,8 @@ public class WeatherException extends RuntimeException {
     public WeatherException(String message) {
         super(message);
     }
+
+    public WeatherException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
